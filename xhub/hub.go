9aa916494ba164956c4baf0967553137da8d7039
aronfan <aronfan@vip.qq.com>
2024-03-10 18:55:42 +0800

    xhub commit

---
+++ xhub/hub.go
@@ -0,0 +1,104 @@
+package xhub
+
+import "sync"
+
+type MessageHub struct {
+	lock sync.RWMutex
+	subs map[string][]*MessageSub
+}
+
+func NewMessageHub() *MessageHub {
+	return &MessageHub{
+		lock: sync.RWMutex{},
+		subs: make(map[string][]*MessageSub),
+	}
+}
+
+func (hub *MessageHub) Add(key string, sub *MessageSub) {
+	if key == "" || sub == nil {
+		return
+	}
+
+	hub.lock.Lock()
+	defer hub.lock.Unlock()
+
+	subList, ok := hub.subs[key]
+	if ok {
+		idx := -1
+		for i := 0; i < len(subList); i++ {
+			if subList[i] == sub {
+				idx = i
+				break
+			}
+		}
+		if idx == -1 {
+			subList = append(subList, sub)
+			hub.subs[key] = subList
+		}
+	} else {
+		subList = append(subList, sub)
+		hub.subs[key] = subList
+	}
+}
+
+func (hub *MessageHub) Del(key string, sub *MessageSub) {
+	if key == "" || sub == nil {
+		return
+	}
+
+	hub.lock.Lock()
+	defer hub.lock.Unlock()
+
+	subList, ok := hub.subs[key]
+	if ok {
+		if len(subList) == 1 && subList[0] == sub {
+			delete(hub.subs, key)
+		} else {
+			idx := -1
+			for i := 0; i < len(subList); i++ {
+				if subList[i] == sub {
+					idx = i
+					break
+				}
+			}
+			if idx != -1 {
+				newList := append(subList[:idx], subList[idx+1:]...)
+				hub.subs[key] = newList
+			}
+		}
+	}
+}
+
+func (hub *MessageHub) Pub(key string, msg string) {
+	if key == "" || msg == "" {
+		return
+	}
+
+	hub.lock.Lock()
+	defer hub.lock.Unlock()
+
+	subList, ok := hub.subs[key]
+	if ok {
+		for i := 0; i < len(subList); i++ {
+			sub := subList[i]
+			sub.Pub(msg)
+		}
+	}
+}
+
+func (hub *MessageHub) Stop(key string) {
+	if key == "" {
+		return
+	}
+
+	hub.lock.Lock()
+	defer hub.lock.Unlock()
+
+	subList, ok := hub.subs[key]
+	if ok {
+		for i := 0; i < len(subList); i++ {
+			sub := subList[i]
+			sub.Stop()
+		}
+	}
+}
---
+++ xhub/sub.go
@@ -0,0 +1,62 @@
+package xhub
+
+import "sync"
+
+type MessageSub struct {
+	key    string
+	cb     func(msg any) error
+	ch     chan any
+	initWG sync.WaitGroup
+	finiWG sync.WaitGroup
+}
+
+func NewMessageSub(key string, cb func(any) error) *MessageSub {
+	return &MessageSub{
+		key:    key,
+		cb:     cb,
+		ch:     make(chan any),
+		initWG: sync.WaitGroup{},
+		finiWG: sync.WaitGroup{},
+	}
+}
+
+func (sub *MessageSub) Start() {
+	sub.initWG.Add(1)
+	sub.finiWG.Add(1)
+
+	go func() {
+		defer func() {
+			sub.finiWG.Done()
+		}()
+
+		sub.initWG.Done()
+
+	OuterLoop:
+		for {
+			select {
+			case msg, ok := <-sub.ch:
+				if !ok {
+					break OuterLoop
+				}
+				err := sub.cb(msg)
+				if err != nil {
+					break OuterLoop
+				}
+			}
+		}
+	}()
+
+	sub.initWG.Wait()
+}
+
+func (sub *MessageSub) Stop() {
+	close(sub.ch)
+}
+
+func (sub *MessageSub) Wait() {
+	sub.finiWG.Wait()
+}
+
+func (sub *MessageSub) Pub(msg any) {
+	sub.ch <- msg
+}
