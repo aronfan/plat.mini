69908bec5841d43f52c4b8795ec0db1880cc5541
aronfan <aronfan@vip.qq.com>
2025-02-26 22:11:05 +0800

    add mail pick api

--- xdb/mail.go
+++ xdb/mail.go
@@ -3,6 +3,7 @@ package xdb
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 
 	"github.com/aronfan/plat.mini/xlog"
 	"go.uber.org/zap"
@@ -24,20 +25,22 @@ const (
 	MailReceiveTypeUnlimit = "unlimit"
 )
 
-type Mail struct {
-	gorm.Model `json:"-"`
+type AttachmentsType map[string]any
 
-	UserID            int64          `json:"user_id" gorm:"not null"`
-	VisibleType       string         `json:"visible_type" gorm:"column:visible_type;type:varchar(128);not null"`
-	SourceType        string         `json:"source_type" gorm:"type:varchar(128);not null"`
-	SourceUuid        string         `json:"source_uuid" gorm:"type:varchar(255);not null"`
-	ReceiveType       string         `json:"receive_type" gorm:"column:receive_type;type:varchar(128);index:idx_receive_type_uuid;not null"`
-	ReceiveUuid       string         `json:"receive_uuid" gorm:"column:receive_uuid;type:varchar(255);index:idx_receive_type_uuid"`
-	Subject           string         `json:"subject" gorm:"type:text;not null"`
-	Body              string         `json:"body" gorm:"type:text;"`
-	Attachments       map[string]any `json:"attachments" gorm:"-"`
-	AttachmentsJSON   string         `json:"-" gorm:"type:json"`
-	AttachmentsPicked bool           `json:"-" gorm:""`
+type Mail struct {
+	gorm.Model `json:"model"`
+
+	UserID            int64           `json:"user_id" gorm:"column:user_id;not null"`
+	VisibleType       string          `json:"visible_type" gorm:"column:visible_type;type:varchar(128);not null"`
+	SourceType        string          `json:"source_type" gorm:"type:varchar(128);not null"`
+	SourceUuid        string          `json:"source_uuid" gorm:"type:varchar(255);not null"`
+	ReceiveType       string          `json:"receive_type" gorm:"column:receive_type;type:varchar(128);index:idx_receive_type_uuid;not null"`
+	ReceiveUuid       string          `json:"receive_uuid" gorm:"column:receive_uuid;type:varchar(255);index:idx_receive_type_uuid"`
+	Subject           string          `json:"subject" gorm:"type:text;not null"`
+	Body              string          `json:"body" gorm:"type:text;"`
+	AttachmentsJSON   string          `json:"-" gorm:"type:json"`
+	AttachmentsPicked bool            `json:"-" gorm:"column:attachments_picked"`
+	Attachments       AttachmentsType `json:"attachments" gorm:"-"`
 }
 
 func InsertMail(ydb *gorm.DB, mail *Mail) (int64, error) {
@@ -83,20 +86,51 @@ func DeleteMail(ydb *gorm.DB, userID, mailID int64) (int64, error) {
 	return ydb.RowsAffected, nil
 }
 
-func GetMailList(ydb *gorm.DB, userID int64, offset, limit int, seeall bool) ([]*Mail, error) {
+func GetMailList(ydb *gorm.DB, userID int64, offset, limit int, order string, seeall bool) ([]*Mail, error) {
 	var mails []*Mail
 	query := ydb.Where("user_id = ?", userID)
 	if !seeall {
 		query = query.Where("visible_type = ?", MailVisibleNormal)
 	}
-	err := query.Offset(offset).Limit(limit).Find(&mails).Error
+	err := query.Offset(offset).Limit(limit).Order(order).Find(&mails).Error
 	if err != nil {
 		return nil, err
 	}
 	return mails, nil
 }
 
-func PickMailAttachments(ydb *gorm.DB, userID, mailID int64) error {
-	// TODO
-	return nil
+func PickMailAttachments(ydb *gorm.DB, userID, mailID int64,
+	handleAttachments func(attachments AttachmentsType) error) error {
+	return ydb.Transaction(func(tx *gorm.DB) error {
+		mail := &Mail{
+			UserID: userID,
+			Model:  gorm.Model{ID: uint(mailID)},
+		}
+		err := ydb.First(mail).Error
+		if err != nil {
+			return err
+		}
+		if mail.AttachmentsPicked {
+			return fmt.Errorf("attachments already picked for mail=%d", mailID)
+		}
+
+		var attachments AttachmentsType
+		err = json.Unmarshal([]byte(mail.AttachmentsJSON), &attachments)
+		if err != nil {
+			return fmt.Errorf("failed to parse attachments JSON for mail=%d: %v", mailID, err)
+		}
+
+		err = tx.Model(&Mail{}).Where("user_id = ? AND id = ?", userID, mailID).
+			Update("attachments_picked", true).Error
+		if err != nil {
+			return err
+		}
+
+		err = handleAttachments(attachments)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
 }
